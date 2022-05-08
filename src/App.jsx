b8dbbe2367c4ed3837850136a61d3f2e858afaa2
Ulises Lara <lara_ulises_@outlook.com>
2022-05-07 20:36:05 -0500

    Refactor: Move message construction logic to  'render' method

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
-const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-const message = helloContinents.join(" ");
-
 class HelloWorld extends React.Component {
     render() {
+        const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
+        const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+        const message = helloContinents.join(" ");
+
         return (
             <div title="Outer div">
                 <h1>{message}</h1>
