073ddc1d81d5be9ac2150befa11c77d8fe360866
Ulises Lara <lara_ulises_@outlook.com>
2022-05-07 21:51:32 -0500

    Remove old 'HelloWorld' component class, now unused

--- src/App.jsx
+++ src/App.jsx
@@ -37,20 +37,6 @@ class IssueList extends React.Component {
     }
 }
 
-class HelloWorld extends React.Component {
-    render() {
-        const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
-        const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-        const message = helloContinents.join(" ");
-
-        return (
-            <div title="Outer div">
-                <h1>{message}</h1>
-            </div>
-        );
-    }
-}
-
 const element = <IssueList />;
 
 ReactDOM.render(element, document.getElementById("content"));
