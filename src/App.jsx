dbee0d9c51fe0987582bacaeed6da3596e6bfca6
Ulises Lara <lara_ulises_@outlook.com>
2022-05-08 16:19:14 -0500

    Render table of issues using 'IssueTable' component

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,21 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
     render() {
+        const rowStyle = { border: "1px solid silver", padding: 4 };
+
         return (
-            <div> This is a placeholder for a table of issues. </div>
+            <table style={{ borderCollapse: "collapse" }}>
+                <thead>
+                    <tr>
+                        <th style={rowStyle}> ID </th>
+                        <th style={rowStyle}> Title </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow rowStyle={rowStyle} issue_id={1} issue_title="Error in console when clicking Add" />
+                    <IssueRow rowStyle={rowStyle} issue_id={2} issue_title="Missing bottom border on panel" />
+                </tbody>
+            </table>
         );
     }
 }
