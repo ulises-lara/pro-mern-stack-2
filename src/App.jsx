51d8bf296781b1164e3a89f395003767a51c2f08
Ulises Lara <lara_ulises_@outlook.com>
2022-05-07 20:33:40 -0500

    Create 'HelloWorld' component class

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
 const helloContinents = Array.from(continents, c => `Hello ${c}!`);
 const message = helloContinents.join(" ");
 
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div title="Outer div">
+                <h1>{message}</h1>
+            </div>
+        );
+    }
+}
+
 const element = (
     <div title="Outer div">
         <h1>{message}</h1>
