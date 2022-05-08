a98720de8ead67937ae634d2f3ae65c3818cf256
Ulises Lara <lara_ulises_@outlook.com>
2022-05-08 16:49:36 -0500

    Create array of issues, to generate table dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+const issues = [
+    {
+        id: 1,
+        status: "New",
+        owner: "Ravan",
+        effort: 5,
+        created: new Date("2018-08-15"),
+        due: undefined,
+        title: "Error in console when clicking Add",
+    },
+    {
+        id: 2,
+        status: "Assigned",
+        owner: "Eddie",
+        effort: 14,
+        created: new Date("2018-08-16"),
+        due: new Date("2018-08-30"),
+        title: "Missing bottom border on panel",
+    },
+];
+
 class IssueFilter extends React.Component {
     render() {
         return (
