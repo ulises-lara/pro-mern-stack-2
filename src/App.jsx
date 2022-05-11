92d789f5188a5e35d9ee4c8aca3247ef564d5edd
Ulises Lara <lara_ulises_@outlook.com>
2022-05-11 18:01:03 -0500

    Add function to handle submit event of 'issueAdd' form

--- src/App.jsx
+++ src/App.jsx
@@ -79,11 +79,27 @@ class IssueRow extends React.Component {
 class IssueAdd extends React.Component {
     constructor() {
         super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        const form = document.forms.issueAdd;
+        const issue = {
+            owner: form.owner.value,
+            title: form.title.value,
+            status: "New",
+        };
+        this.props.createIssue(issue);
+
+        form.owner.value = "";
+        form.title.value = "";
     }
 
     render() {
         return (
-            <form>
+            <form name="issueAdd" onSubmit={this.handleSubmit}>
                 <input type="text" name="owner" placeholder="Owner" />
                 <input type="text" name="title" placeholder="Title" />
                 <button> Add </button>
