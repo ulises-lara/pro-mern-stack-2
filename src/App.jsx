0d08f8cb1bf4fb31912ece8d5f8b016d2e55c126
Ulises Lara <lara_ulises_@outlook.com>
2022-05-17 20:29:04 -0500

    Re-write 'IssueRow' component as function, rather than class

--- src/App.jsx
+++ src/App.jsx
@@ -52,22 +52,20 @@ class IssueTable extends React.Component {
     }
 }
 
-class IssueRow extends React.Component {
-    render() {
-        const issue = this.props.issue;
+function IssueRow(props) {
+    const issue = props.issue;
 
-        return (
-            <tr>
-                <td> {issue.id} </td>
-                <td> {issue.status} </td>
-                <td> {issue.owner} </td>
-                <td> {issue.created.toDateString()} </td>
-                <td> {issue.effort} </td>
-                <td> {issue.due ? issue.due.toDateString() : ""} </td>
-                <td> {issue.title} </td>
-            </tr>
-        );
-    }
+    return (
+        <tr>
+            <td> {issue.id} </td>
+            <td> {issue.status} </td>
+            <td> {issue.owner} </td>
+            <td> {issue.created.toDateString()} </td>
+            <td> {issue.effort} </td>
+            <td> {issue.due ? issue.due.toDateString() : ""} </td>
+            <td> {issue.title} </td>
+        </tr>
+    );
 }
 
 class IssueAdd extends React.Component {
