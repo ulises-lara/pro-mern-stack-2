ba9d6f32b518f79635a83dadfb15feda6cbc000c
Ulises Lara <lara_ulises_@outlook.com>
2022-05-17 20:32:15 -0500

    Re-write 'IssueTable' component as function, rather than class

--- src/App.jsx
+++ src/App.jsx
@@ -27,29 +27,27 @@ class IssueFilter extends React.Component {
     }
 }
 
-class IssueTable extends React.Component {
-    render() {
-        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
+function IssueTable(props) {
+    const issueRows = props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
 
-        return (
-            <table className="bordered-table">
-                <thead>
-                    <tr>
-                        <th> ID </th>
-                        <th> Status </th>
-                        <th> Owner </th>
-                        <th> Created </th>
-                        <th> Effort </th>
-                        <th> Due Date </th>
-                        <th> Title </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {issueRows}
-                </tbody>
-            </table>
-        );
-    }
+    return (
+        <table className="bordered-table">
+            <thead>
+                <tr>
+                    <th> ID </th>
+                    <th> Status </th>
+                    <th> Owner </th>
+                    <th> Created </th>
+                    <th> Effort </th>
+                    <th> Due Date </th>
+                    <th> Title </th>
+                </tr>
+            </thead>
+            <tbody>
+                {issueRows}
+            </tbody>
+        </table>
+    );
 }
 
 function IssueRow(props) {
